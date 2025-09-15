a3a056e4a7b11853fd55c5c0639068fd038ee353
apiload5 <amir03182055632@gmail.com>
2025-09-16 00:44:25 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'game_screen.dart'; // Yakeeni banayen ke yeh line maujood hai
 
 // Your AdMob App ID
 const String adMobAppId = "ca-app-pub-9525665139626448~1351897389";
@@ -16,10 +16,6 @@ const String adTask = "show_ad_task";
 @pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
-    // Implement logic to show ad
-    // NOTE: Direct UI interaction is not possible from a background task.
-    // This task is meant to signal the app to show an ad when it's active.
-    // We will use a flag in shared preferences for this.
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('showAdNow', true);
     return Future.value(true);
@@ -33,11 +29,6 @@ void main() async {
     callbackDispatcher,
     isInDebugMode: false,
   );
-  runApp(const MyApp());
-  _scheduleAdTask();
-}
-
-void _scheduleAdTask() {
   Workmanager().registerPeriodicTask(
     "periodic-ad-task",
     adTask,
@@ -46,74 +37,16 @@ void _scheduleAdTask() {
       networkType: NetworkType.connected,
     ),
   );
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  InterstitialAd? _interstitialAd;
-  bool _isAdLoaded = false;
-  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadInterstitialAd();
-    _checkAndShowAd();
-    Timer.periodic(const Duration(minutes: 5), (timer) {
-      _checkAndShowAd();
-    });
-  }
-
-  void _loadInterstitialAd() {
-    InterstitialAd.load(
-      adUnitId: gameInterstitialAdUnitId,
-      request: const AdRequest(),
-      adLoadCallback: InterstitialAdLoadCallback(
-        onAdLoaded: (InterstitialAd ad) {
-          _interstitialAd = ad;
-          _isAdLoaded = true;
-          ad.fullScreenContentCallback = FullScreenContentCallback(
-            onAdDismissedFullScreenContent: (InterstitialAd ad) {
-              ad.dispose();
-              _loadInterstitialAd();
-            },
-            onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
-              ad.dispose();
-              _loadInterstitialAd();
-            },
-          );
-        },
-        onAdFailedToLoad: (LoadAdError error) {
-          _isAdLoaded = false;
-        },
-      ),
-    );
-  }
-
-  void _checkAndShowAd() async {
-    final prefs = await SharedPreferences.getInstance();
-    bool showAd = prefs.getBool('showAdNow') ?? false;
-    if (showAd && _isAdLoaded) {
-      _interstitialAd?.show();
-      await prefs.setBool('showAdNow', false);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: navigatorKey,
-      home: const GameScreen(),
+    return const MaterialApp(
+      home: GameScreen(), // Yahan 'const' ka istemal sahi hai
     );
   }
 }
-
-// Yahan par baaki game screen ka code
-// (lib/game_screen.dart file se)
-// ...
